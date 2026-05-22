d9ea45c8c0f98f2e6a3ca7fca61be3dbcc9f5eb2
Lorenzo <lorvillarivera@gmail.com>
2026-05-22 12:29:03 +0100

    Add semicolon#

--- src/js/index.js
+++ src/js/index.js
@@ -2,7 +2,7 @@
 //import './modules/toggle';
 //import './modules/app';
 //import './modules/todo copy';
-import './modules/game'
+import './modules/game';
 
 // import(/* webpackChunkName: "toggle" */`./modules/toggle`).then(module => module.default());
 // import(/* webpackChunkName: "validate" */`@stormid/validate`).then(module => module.default.init(VALIDATE.SELECTOR));
